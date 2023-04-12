83c8597dcb66dda1904ae003629ef091e9ccf5f9
Michael Norris <imichaelnorris@gmail.com>
2023-04-11 19:08:59 -0700

    Update mcspeedy.js
    optional refactoring + adding key listener

--- mcspeedy.js
+++ mcspeedy.js
@@ -7,69 +7,101 @@ javascript:(function() {
   const currentSpeed = video.playbackRate;
   const currentIndex = ticks.indexOf(currentSpeed);
 
-  const popup = document.createElement('div');
-  popup.style.position = 'fixed';
-  popup.style.top = '50%';
-  popup.style.left = '50%';
-  popup.style.transform = 'translateX(-50%)';
-  popup.style.zIndex = 1000;
-  popup.style.padding = '1em';
-  popup.style.backgroundColor = '#FFF';
-  popup.style.border = '1px solid #000';
-  popup.style.textAlign = 'center';
-  popup.style.cursor = 'pointer';
-
-  const input = document.createElement('input');
-  input.type = 'range';
-  input.min = 0;
-  input.max = ticks.length - 1;
-  input.step = 1;
-  input.value = currentIndex != -1 ? currentIndex : 1;
-  input.style.flexGrow = "1";
-  input.setAttribute("list", "steplist");
-  popup.appendChild(input);
-
-  const datalist = document.createElement('datalist');
-  datalist.id = "steplist";
-  for (var i = 0; i < ticks.length; i++) {
-    const option = document.createElement('option');
-    option.innerText = i;
-    datalist.appendChild(option);
+  function createPopup() {
+    const popup = document.createElement('div');
+    popup.style.position = 'fixed';
+    popup.style.top = '50%';
+    popup.style.left = '50%';
+    popup.style.transform = 'translateX(-50%)';
+    popup.style.zIndex = 1000;
+    popup.style.padding = '1em';
+    popup.style.backgroundColor = '#FFF';
+    popup.style.border = '1px solid #000';
+    popup.style.textAlign = 'center';
+    popup.style.cursor = 'pointer';
+    return popup;
   }
-  popup.appendChild(datalist);
+  
+  function createInput() {
+    const input = document.createElement('input');
+    input.type = 'range';
+    input.min = 0;
+    input.max = ticks.length - 1;
+    input.step = 1;
+    input.value = currentIndex != -1 ? currentIndex : 1;
+    input.style.flexGrow = "1";
+    input.setAttribute("list", "steplist");
+    popup.appendChild(input);
+    return input;
+  }
 
-  const labels = document.createElement('div');
-  labels.style.display = 'flex';
-  labels.style.justifyContent = 'space-between';
-  popup.appendChild(labels);
+  function createDatalist() {
+    const datalist = document.createElement('datalist');
+    datalist.id = "steplist";
+    for (var i = 0; i < ticks.length; i++) {
+      const option = document.createElement('option');
+      option.innerText = i;
+      datalist.appendChild(option);
+    }
+    popup.appendChild(datalist);
+    return datalist;
+  }
 
-  ticks.forEach(tick => {
-    const label = document.createElement('div');
-    label.innerText = tick;
-    label.style.flex = '1';
-    label.style.textAlign = 'center';
-    label.style.width = `${100 / ticks.length}%`;
-    label.style.transform = 'rotate(90deg)';
-    
-    label.addEventListener('mousedown', () => {
-      input.value = ticks.indexOf(tick);
-      video.playbackRate = tick;
+  function createLabels() {
+    const labels = document.createElement('div');
+    labels.style.display = 'flex';
+    labels.style.justifyContent = 'space-between';
+    popup.appendChild(labels);
+    return labels;
+  }
+  
+  function createTicks(ticks, labels) {
+    ticks.forEach(tick => {
+      const label = document.createElement('div');
+      label.innerText = tick;
+      label.style.flex = '1';
+      label.style.textAlign = 'center';
+      label.style.width = `${100 / ticks.length}%`;
+      label.style.transform = 'rotate(90deg)';
+
+      label.addEventListener('mousedown', () => {
+        input.value = ticks.indexOf(tick);
+        video.playbackRate = tick;
+      });
+      label.addEventListener('mouseup', () => {
+        document.body.removeChild(popup);
+      });
+      labels.appendChild(label);
     });
-    label.addEventListener('mouseup', () => {
+  }
+
+  function onInput(input) {
+    input.addEventListener('input', () => {
+      video.playbackRate = ticks[input.value];
+    });
+    
+    input.addEventListener('mouseup', () => {
       document.body.removeChild(popup);
     });
-    labels.appendChild(label);
-  });
-
-  input.addEventListener('input', () => {
-    video.playbackRate = ticks[input.value];
-  });
-
-  input.addEventListener('mouseup', () => {
-    document.body.removeChild(popup);
-  });
-
-  input.style.width = `400px`;
+    
+    input.style.width = `400px`;
+  }
+  
+  function keyListener() {
+     popup.addEventListener('keydown', (evt) => {
+       evt.preventDefault();
+       console.log(evt)
+     });
+     popup.focus();
+  }
+  
+  const popup = createPopup();
+  const input = createInput();
+  const datalist = createDatalist();
+  const labels = createLabels();
+  createTicks(ticks, labels);
+  onInput(input);
+  keyListener();
 
   document.body.appendChild(popup);
 })();
